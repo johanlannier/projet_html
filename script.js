a5d84587f819f240a6c6187aa77db0ee24e360c3
NQuillon <nic.quillon@gmail.com>
2017-03-29 09:25:05 +0200

    NOIREEEEEEEE

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 			if((i+j)%2 == 0){
 				html += "<td class=blanche>";
 			}else{
-				html += "<td class=noir>";
+				html += "<td class=noire>";
 			}
 		}
 		html += "</tr>";
