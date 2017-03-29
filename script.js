5a4aa3eff37b478bd2d102c0024d5e37d042b10e
NQuillon <nic.quillon@gmail.com>
2017-03-29 09:07:23 +0200

    debut plateau

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 $(document).ready(function(){
-	
-	for(var i=0; i<10; i++){
-		for(var j=0; j<10; j++){
 
+	var html = "<table>";
+	for(var i=0; i<10; i++){
+		html += "<tr>";
+		for(var j=0; j<10; j++){
+			if((i+j)%2 == 0){
+				html += "<td class=blanche>";
+			}else{
+				html += "<td class=noir>";
+			}
 		}
+		html += "</tr>";
 	}
+	html += "</table>";
+
+	$("#plateau").
 
 
 
