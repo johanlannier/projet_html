067a4f25147baf3ad79481e2d95ffacc989b4a62
NQuillon <nic.quillon@gmail.com>
2017-03-29 09:23:55 +0200

    plateau

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	var html = "<table>";
+	var html = "<table class=plateau>";
 	for(var i=0; i<10; i++){
 		html += "<tr>";
 		for(var j=0; j<10; j++){
@@ -14,7 +14,7 @@ $(document).ready(function(){
 	}
 	html += "</table>";
 
-	$("#plateau").
+	$("#plateau").append(html);
 
 
 
